612db358c3e8e0b1da10309545d75fbd25515ae4
vcLau <vclau1996@163.com>
2023-11-26 21:58:18 +0800

    特殊符号转换中正则可动态设置

--- src/pages/MarkTransform/index.tsx
+++ src/pages/MarkTransform/index.tsx
@@ -1,7 +1,7 @@
 /**
  * 特殊符号转化
  * 规则：
- * 1. 默认将';'、','、'\n'转换为'|'
+ * 1. 默认将';'、','、'\r\n'转换为'|'
  * 2. 转换后去除所有空格
  */
 
@@ -9,19 +9,36 @@ import { Input, message } from "antd";
 import copy from "copy-to-clipboard";
 import { memo, useCallback, useMemo, useState } from "react";
 
-const MARK_REG = /;|,|[\r\n]| +/g;
-const TARGET_MARK = "-";
+/** 默认正则匹配规则 匹配';'、','、'\r\n'、' ' */
+const MARK_REG = ";|,|[\r\n]| +";
+/** 默认转换的目标符号 */
+const TARGET_MARK = "|";
 
 const MarkTransform = () => {
   const [value, setValue] = useState("");
+  const [specialMark, setSpecialMark] = useState("");
+
+  /** 动态正则规则，用户可自行配置特殊符号 */
+  const regExp = useMemo(() => {
+    try {
+      const result = new RegExp(
+        MARK_REG + (specialMark ? `|${specialMark}` : ""),
+        "g"
+      );
+      return result;
+    } catch (e) {
+      message.error("该特殊符号不可转换，请联系管理员");
+      return new RegExp(MARK_REG, "g");
+    }
+  }, [specialMark]);
 
   /** 根据正则对文本进行格式化 */
   const formateValue = useMemo(() => {
     if (!value) return "";
-    const result = value.replace(MARK_REG, TARGET_MARK).replace(/-+/g, "-");
+    const result = value.replace(regExp, TARGET_MARK).replace(/-+/g, "-");
 
     return result;
-  }, [value]);
+  }, [value, regExp]);
 
   /** 一键复制备注 */
   const copyRemark = useCallback(() => {
@@ -42,6 +59,15 @@ const MarkTransform = () => {
           rows={6}
           onChange={(e) => {
             setValue(e.target.value);
+          }}
+        />
+      </div>
+      <div className="mb-6 flex items-center">
+        自定义符号：
+        <Input
+          className="w-[150px]"
+          onChange={(e) => {
+            setSpecialMark(e.target.value);
           }}
         />
       </div>
